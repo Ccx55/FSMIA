938ec45803c50115b2717187882e13ef82294b72
HSH <seas363@gmail.com>
2014-06-29 22:00:17 -0400

    add function for animating trajectory

--- analyzeDiffusion.m
+++ analyzeDiffusion.m
@@ -1,33 +1,47 @@
-function result = analyzeDiffusion(trajall,Molecule,Option)
-% Calculate diffusion coefficient
+function result = analyzeDiffusion(trajall,Molecule,Option,traj_ind)
+% Analyze diffusion
 % Input
-%   TRAJALL - trajectory results from previous image analysis
-% Output
-%   RESULT.X - (r^2/4\delta t) in ascending order
-%   RESULT.Y - Cumulative distribution function
+%   trajall - trajectory results from previous image analysis
+%   traj_ind - index of trajectory
+% $Revision: 2.0 $  $Date: 2014/06/29 $
 
-opt = struct;
-opt.n = 4;
+traj = trajall(traj_ind).trajectory;
+steps = computestep(traj,Molecule,Option);
+result = steps;
 
-allSteps = [];
-for i = 1:length(trajall)
-    traj = trajall(i).trajectory;
-    steps = computestep(traj,Molecule,Option,opt);
-    allSteps = vertcat(allSteps,steps);
-end
 
-% ocnvert to r^2/8dt (um^2/s)
-allSteps = (allSteps*1e-3).^2/(4*Option.ExposureTime*1e-3*opt.n);
-allStepsAscend = sort(allSteps);
-
-% number of observed displacements
-M = length(allStepsAscend);
-% cumulative distribution distribution
-% use Schwartz's definition, it's actually 1-c.d.f.
-cdf = 1 - (1:M)/M;
-
-% plot(allStepsAscend,cdf)
-
-result = struct;
-result.x = allStepsAscend;
-result.y = cdf;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%HISTORY%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Calculate diffusion coefficient Input
+% %   TRAJALL - trajectory results from previous image analysis
+% % Output
+% %   RESULT.X - (r^2/4\delta t) in ascending order RESULT.Y - Cumulative
+% %   distribution function
+% % $Revision: 1.0 $  $Date: 2014/05/07 $
+% 
+% opt = struct;
+% opt.n = 4;
+% 
+% allSteps = [];
+% for i = 1:length(trajall)
+%     traj = trajall(i).trajectory;
+%     steps = computestep(traj,Molecule,Option,opt);
+%     allSteps = vertcat(allSteps,steps);
+% end
+% 
+% % ocnvert to r^2/8dt (um^2/s)
+% allSteps = (allSteps*1e-3).^2/(4*Option.ExposureTime*1e-3*opt.n);
+% allStepsAscend = sort(allSteps);
+% 
+% % number of observed displacements
+% M = length(allStepsAscend);
+% % cumulative distribution distribution use Schwartz's definition, it's
+% % actually 1-c.d.f.
+% cdf = 1 - (1:M)/M;
+% 
+% % plot(allStepsAscend,cdf)
+% 
+% result = struct;
+% result.x = allStepsAscend;
+% result.y = cdf;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ creatTrajectoryMovie.m
@@ -0,0 +1,54 @@
+function creatTrajectoryMovie(image_file,trajall,Molecule,traj_ind)
+% Create movie to visulize a single trajectory
+% Input:
+%   image_file - complete file name of the analyzed image
+%   trajall - trajectory results from previous image analysis
+%   traj_ind - index of trajectory
+% Output:
+%   An tiff file of selected trajectory in the same folder as that of
+%   image_file
+% $Revision: 1.0 $  $Date: 2014/06/29 $
+
+traj = trajall(traj_ind).trajectory;
+frame_appear = Molecule(traj(1)).frame;
+frame_disappear = Molecule(traj(end)).frame;
+info = imfinfo(image_file);
+n_images = numel(info);
+[pathstr,~,~] = fileparts(file);
+% Output file name
+ofile_name = fullfile(pathstr,(strcat('Trajectory_',num2str(traj_ind),...
+    '.tiff')));
+
+for i = 1:(frame_appear-1)
+    im = imread(image_file,i);
+    RGB = repmat(im,[1,1,3]);
+    if i == 1
+        imwrite(RGB,ofile_name);
+    else
+        imwrite(RGB,ofile_name,'WriteMode','append');
+    end
+end
+
+j = 1;
+
+for i = frame_appear:frame_disappear
+    im = imread(image_file,i);
+    % locate 'point'
+    pt = Molecule(traj(j)).coordinates(1:2);
+    pt = int32(pt);
+    marker_intensity = int16(max(max(im)));
+    RGB(pt(1)-2:pt(1)+2,pt(2),1) = 0;
+    RGB(pt(1)-2:pt(1)+2,pt(2),2) = marker_intensity;
+    RGB(pt(1)-2:pt(1)+2,pt(2),3) = 0;
+    RGB(pt(1),pt(2)-2:pt(2)+2,1) = 0;
+    RGB(pt(1),pt(2)-2:pt(2)+2,2) = marker_intensity;
+    RGB(pt(1),pt(2)-2:pt(2)+2,3) = 0;
+    imwrite(RGB,ofile_name,'WriteMode','append');
+    j = j + 1;
+end
+
+for i = (frame_disappear+1):n_images
+    im = imread(image_file,i);
+    RGB = repmat(im,[1,1,3]);
+    imwrite(RGB,ofile_name,'WriteMode','append');
+end
